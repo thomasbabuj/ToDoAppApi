b3b206609dae0c2a463f2ac439d592b5d1d864e8
Thomas Babu <thomas@interuptive.com>
2015-09-22 17:00:23 +0800

    Added a basic router using Mux

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 /**
-*  A Basic Web server
+*  1) A Basic Web server
+*  2) Adding a Router ( using Mux )
+*		i ) use go get to import external package
+*		ii) we create a basic router, adds the route "/"
+*           and assign the index handler to run when the endpoint is called
 *
  */
 
@@ -10,12 +14,16 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
