f1e0f9a662acd256540662638a9fcd789fcd060d
Thomas Babu <thomas@interuptive.com>
2015-09-23 11:17:46 +0800

    Code refactoring

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ToDoAppApi
 Making a RESTful JSON API in Go
+
+ 1) A Basic Web server
+ 2) Adding a Router ( using Mux )
+		i ) use go get to import external package
+		ii) we create a basic router, adds the route "/"
+           and assign the index handler to run when the endpoint is called
+ 3)  Adding additional basic router
+ 4)  A basic Todo model
+		i) In Golang, a Struct will typically serve as model.
+ 5)  Creating idiomatic JSON
+		i) By using the struct tags we can control exactly how our
+		   struct will be marshalled to JSON
+ 6)  Code refactoring
+
--- main.go
+++ main.go
@@ -1,65 +1,12 @@
-/**
-*  1) A Basic Web server
-*  2) Adding a Router ( using Mux )
-*		i ) use go get to import external package
-*		ii) we create a basic router, adds the route "/"
-*           and assign the index handler to run when the endpoint is called
-* 3)  Adding additional basic router
-* 4)  A basic Todo model
-*		i) In Golang, a Struct will typically serve as model.
-* 5)  Creating idiomatic JSON
-		i) By using the struct tags we can control exactly how our
-		   struct will be marshalled to JSON
-*/
-
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"time"
-
-	"github.com/gorilla/mux"
 )
 
-type Todo struct {
-	Name      string    `json:"name"`
-	Completed bool      `json:"completed"`
-	Due       time.Time `json:"due"`
-}
-
-type Todos []Todo // Todos is a slice of type Todo
-
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-
-	router.HandleFunc("/", Index)
-	router.HandleFunc("/todos", TodoIndex)
-	router.HandleFunc("/todos/{todoId}", TodoShow)
+	router := NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome!")
-}
-
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	// Send back some JSOn
-	// Simulate a real response and mock out the TodoIndex with static
-	// data
-	todos := Todos{
-		Todo{Name: "Write presenation"},
-		Todo{Name: "Host meetup"},
-	}
-
-	json.NewEncoder(w).Encode(todos)
-}
-
-func TodoShow(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	todoId := vars["todoId"]
-	fmt.Fprintln(w, "Todo show:", todoId)
 }
